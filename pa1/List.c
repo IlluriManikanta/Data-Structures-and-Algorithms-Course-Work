938b3cf71204558dc807a90834bcc88b9ff89b52
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-04-09 13:00:31 -0700

    Adding list.c file

---
+++ pa1/List.c
@@ -0,0 +1,12 @@
+// ========== Name ========== //
+// Manikantanagasai H. Illuri //
+// milluri@ucsc.edu           //
+// 2023 Spring CSE101         //
+// PA 1                       //
+// Implementing List ADT      //
+// ========================== //
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "list.h"
