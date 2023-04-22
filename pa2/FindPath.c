6d534864b63108d71bcb1f88c2ee997aade555e8
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-04-22 06:59:55 -0700

    f

--- pa2/FindPath.c
+++ pa2/FindPath.c
@@ -72,70 +72,27 @@ for (;;) {
     getPath(path, G, z);
     int distance = getDist(G, z);
     fprintf(out_file, "The distance from %d to %d is ", y, z);
-    if (distance == INF) {
-        fprintf(out_file, "infinity\n");
-        fprintf(out_file, "No %d-%d path exists\n", y, z);
-    } else {
+    if (distance != INF) {
         fprintf(out_file, "%d\n", distance);
         fprintf(out_file, "A shortest %d-%d path is: ", y, z);
         moveNext(path);
         printList(out_file, path);
+        
+    } else {
+        fprintf(out_file, "infinity\n");
+        fprintf(out_file, "No %d-%d path exists\n", y, z);
     }
     fprintf(out_file, "\n");
 }
 
-// clean up
-freeList(&path);
+//Freeing Graph and List
 freeGraph(&G);
+freeList(&path);
+
+//Closing input and output files
 fclose(in_file);
 fclose(out_file);
+
 return 0;
-
-    // fscanf(in_file, "%d", &x);
-    // Graph Graph = newGraph(x);
-    // for (; fscanf(in_file, "%d %d", &y, &z) == 2; ) {
-    //     if (x != 0 && z != 0) {
-    //         addEdge(Graph, y, z);
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // printGraph(out_file, Graph);
-    // fprintf(out_file, "\n");
-    // List p = newList();
-    // for (; fscanf(in_file, "%d %d", &y, &z) == 2; ) {
-    //     if (y == 0 && z == 0) {
-    //         break;
-    //     }
-    // }
-
-    // BFS(Graph, y);
-    //     clear(p);
-    //     getPath(p, Graph, z);
-    //     // int distance = ;
-    //     fprintf(out_file, "The distance from %d to %d is ", y, z);
-    //     if (getDist(Graph, z) != -1) {
-    //         fprintf(out_file, "%d\n", getDist(Graph, z));
-    //         fprintf(out_file, "A shortest %d-%d path is: ", y, z);
-    //         moveNext(p);
-    //         printList(out_file, p);
-
-            
-    //     } else {
-    //         fprintf(out_file, "infinity\n");
-    //         fprintf(out_file, "No %d-%d path exists\n", y, z);
-
-            
-    //     }
-    //     fprintf(out_file, "\n");
-        
-    
-    // freeGraph(&Graph);
-    // freeList(&p);
-
-    // fclose(in_file);
-    // fclose(out_file);
-    // return 0;
 
 }
