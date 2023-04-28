dbe3066044f5b047150eb12330cad9f650df0c28
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-04-28 15:05:31 -0700

    g

--- pa3/GraphTest.c
+++ pa3/GraphTest.c
@@ -1,7 +1,14 @@
-//-----------------------------------------------------------------------------
-// GraphClient.c
-// Test client for the Graph ADT
-//-----------------------------------------------------------------------------
+// ========== Name ========== //
+// Manikantanagasai H. Illuri //
+// milluri@ucsc.edu           //
+// 2023 Spring CSE101         //
+// PA 3                       //
+// Testing ADT Functions      //
+// ========================== //
+// I have added more of my own tests to 
+/****************************************************************************************
+*  GraphTest.c
+*****************************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
 #include"List.h"
@@ -14,27 +21,39 @@ int main(int argc, char* argv[]){
    Graph G = newGraph(n);
    Graph T=NULL;
    Graph C=NULL;
-
+// Build graph G 
    for(i=1; i<=n; i++) append(S, i);
-
+printf("%s\n", "AddEdge test");
    addArc(G, 1,2);
    addArc(G, 1,5);
    addArc(G, 2,5);
    addArc(G, 2,6);
+
    addArc(G, 3,2);
    addArc(G, 3,4);
    addArc(G, 3,6);
    addArc(G, 3,7);
    addArc(G, 3,8);
    addArc(G, 6,5);
+
+
+   addArc(G, 6,9);
+   addArc(G, 10,11);
+   addArc(G, 4,3);
+
    addArc(G, 6,7);
    addArc(G, 8,4);
    addArc(G, 8,7);
+   printf("%s\n", "AddEdge test DONE");
+   
+   // Print adjacency list representation of G
+   printf("%s\n", " Print adjacency list representation of Graph DONE");
    printGraph(stdout, G);
 
    DFS(G, S);
    fprintf(stdout, "\n");
    fprintf(stdout, "x:  d  f  p\n");
+    // Calculate the eccentricity of each finish and discover
    for(i=1; i<=n; i++){
       fprintf(stdout, "%d: %2d %2d %2d\n", i, getDiscover(G, i), getFinish(G, i), getParent(G, i));
    }
@@ -60,6 +79,8 @@ int main(int argc, char* argv[]){
    printList(stdout, S);
    fprintf(stdout, "\n");
 
+
+   // Free objects
    freeList(&S);
    freeGraph(&G);
    freeGraph(&T);
