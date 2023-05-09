481acb89faa0ed318f772fce66e89dcffb475a02
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-05-09 14:13:59 -0700

    fsda

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -329,9 +329,11 @@ Matrix sum(Matrix A, Matrix B){
         for(int i = 1; i <= size(A); i++){
            List a_row;
            List b_row;
+           List sum_list;
 
            a_row = A->rows[i];
            b_row = B->rows[i];
+           sum_list = s_matrix->rows[i];
 
            moveFront(a_row);
            moveFront(b_row);
@@ -339,29 +341,34 @@ Matrix sum(Matrix A, Matrix B){
                 
                 Entry a = (Entry)get(a_row);
                 Entry b = (Entry)get(b_row);
+
                 if(a->column == b->column){  
                     Entry temp = newEntry(i, a->value + b->value); 
-                    append(a_row, temp);
+                    append(sum_list, temp);
+                    s_matrix->NNZ++;
                     //changeEntry(s_matrix, i, a->column, a->value + b->value);
                     moveNext(a_row);
-
                     moveNext(b_row);
                 } else if(a->column < b->column){
                     Entry temp = newEntry(i, a->value); 
-                    append(a_row, temp);
+                    append(sum_list, temp);
+                    s_matrix->NNZ++;
                     //changeEntry(s_matrix, i, a->column, a->value);
                     moveNext(a_row);
                 } else {
                     Entry temp = newEntry(i, b->value); 
-                    append(b_row, temp);
+                    append(sum_list, temp);
+                    s_matrix->NNZ++;
+                    moveNext(b_row);
                 }
             }
 
             while(index(a_row) >= 0){
 
                 Entry a = (Entry)get(a_row);
-                Entry temp = newEntry(i, a->value); 
-                append(a_row, temp);
+                Entry temp = newEntry(a->column, a->value); 
+                append(sum_list, temp);
+                s_matrix->NNZ++;
                 //changeEntry(s_matrix, i, a->column, a->value);
                 moveNext(a_row);
             }
@@ -369,8 +376,9 @@ Matrix sum(Matrix A, Matrix B){
             while(index(b_row) >= 0){
 
                 Entry b = (Entry)get(b_row);
-                Entry temp = newEntry(i, b->value); 
-                append(b_row, temp);
+                Entry temp = newEntry(b->column, b->value); 
+                append(sum_list, temp);
+                s_matrix->NNZ++;
                 //changeEntry(s_matrix, i, b->column, b->value);
                 moveNext(b_row);
             }
