9a2cf21286f76702bebac344fe1a77265d9e6db0
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-05-16 16:37:59 -0700

    fas

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -288,9 +288,44 @@ List::~List() {
    // is not moved with respect to the retained elements, i.e. it lies between 
    // the same two retained elements that it did before cleanup() was called.
    void List::cleanup(){
+  Node *n = frontDummy->next;
+  int size = 0;
+  int pos = 0;
+  while (n != backDummy) {
+    Node *t = n->next;
+    while (t != backDummy) {
+      if (n->data != t->data) {
+        t = t->next;
+      } else {
+        if (t == afterCursor) {
+          eraseAfter();
+        } else if (t == beforeCursor) {
+          eraseBefore();
+        } else {
+          Node *d = t;
+          t->next->prev = t->prev;
+          t->prev->next = t->next;
+          t = t->next;
+          delete d;
+        }
+      }
+    }
+    n = n->next;
+  }
+  Node *t2 = frontDummy->next;
+  Node *t3 = frontDummy->next;
+  while (t2 != afterCursor) {
+    pos++;
+    t2 = t2->next;
+  }
+  while (t3 != backDummy) {
+    size++;
+    t3 = t3->next;
+  }
+  num_elements = size;
+  pos_cursor = pos;
+}
 
-   }
- 
    // concat()
    // Returns a new List consisting of the elements of this List, followed by
    // the elements of L. The cursor in the returned List will be at postion 0.
