718f7d32bc57a3e64f2896a59aba54d1f9b80739
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-05-17 02:56:51 -0700

    fds

--- pa5/Shuffle.cpp
+++ pa5/Shuffle.cpp
@@ -7,9 +7,64 @@
 // ========================== //
 
 #include <iostream>
-#include<string>
-#include <sstream>
-#include<fstream>
+#include "List.h"
+
+using namespace std;
+
+void shuffle(List &D){
+    List temp;
+    int d_half = D.length() / 2;
+
+    D.moveFront();
+    for (int i = 0; i < d_half; i++) {
+        temp.insertBefore(D.moveNext());
+        D.eraseBefore();
+    }
+
+    temp.moveFront();
+    for (int i = 0; i < temp.length(); i++) {
+        D.moveNext();
+        D.insertBefore(temp.moveNext());
+    }
+
+}
+
+int main(int argc, char* argv[]){
+    //Checking for correct number of arguments
+    if(argc != 2){
+        cerr << "Usage: " << argv[0] << " <deck size>" << endl;
+        return 1;
+    }
+
+    // int decksize = atoi(argv[1]);
+    if(atoi(argv[1]) <=0){
+        cerr << "Deck size must be a positive integer" << endl;
+        return 1;
+    }
+
+    cout << "deck size\t\tshuffle count\n------------------------------" << endl;
+
+    for(int i = 1; i <= atoi(argv[1]); i++){
+        List d;
+        List copydeck = d;
+
+        for(int j = 1; j <= i; j++){
+            d.insertBefore(j);
+        }
+
+        int cnt = 0;
+        while (!(d.equals(copydeck))){
+            shuffle(d);
+            cnt++;
+            if(d.equals(copydeck)){
+                break;
+            }
+
+        }
+        cout << i << "\t\t\t\t" << cnt << endl;
+    }
+    return 1;
+}
 
 
 
